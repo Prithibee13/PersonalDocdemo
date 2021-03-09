fcc190957790663aa78d49473aade2c6c66591f6
Prithibee13 <66945275+Prithibee13@users.noreply.github.com>
2021-03-09 16:15:47 +0600

    Demo

---
+++ control.js
@@ -0,0 +1,106 @@
+const J = () =>
+{
+    document.getElementById("ss1").style.display = "none"   
+}
+
+const Show1 = () =>
+{
+    let img_src = document.getElementById("img_1").getAttribute("src");
+    
+    document.getElementById("res").setAttribute("src", img_src)
+    document.getElementById("do").setAttribute("href", img_src)
+    document.getElementById("Lorem").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ducimus facilis amet maiores et harum ad dolore modi pariatur laborum tempore doloribus aliquid molestiae, vero expedita beatae alias consequuntur blanditiis!"
+}
+
+const Show2 = () =>
+{
+    let img_src = document.getElementById("img_2").getAttribute("src");
+    
+    document.getElementById("res").setAttribute("src", img_src)
+    document.getElementById("Lorem").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ducimus facilis amet maiores et harum ad dolore modi pariatur laborum tempore doloribus aliquid molestiae, vero expedita beatae alias consequuntur blanditiis!"
+}
+
+const Show3 = () =>
+{
+    let img_src = document.getElementById("img_3").getAttribute("src");
+    
+    document.getElementById("res").setAttribute("src", img_src)
+    document.getElementById("Lorem").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ducimus facilis amet maiores et harum ad dolore modi pariatur laborum tempore doloribus aliquid molestiae, vero expedita beatae alias consequuntur blanditiis!"
+}
+
+const Show4 = () =>
+{
+    let img_src = document.getElementById("img_4").getAttribute("src");
+    
+    document.getElementById("res").setAttribute("src", img_src)
+    document.getElementById("Lorem").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ducimus facilis amet maiores et harum ad dolore modi pariatur laborum tempore doloribus aliquid molestiae, vero expedita beatae alias consequuntur blanditiis!"
+}
+
+const Show5 = () =>
+{
+    let img_src = document.getElementById("img_5").getAttribute("src");
+    
+    document.getElementById("res").setAttribute("src", img_src)
+    document.getElementById("Lorem").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ducimus facilis amet maiores et harum ad dolore modi pariatur laborum tempore doloribus aliquid molestiae, vero expedita beatae alias consequuntur blanditiis!"
+}
+
+
+const Show6 = () =>
+{
+    let img_src = document.getElementById("img_6").getAttribute("src");
+    
+    document.getElementById("res").setAttribute("src", img_src)
+    document.getElementById("Lorem").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ducimus facilis amet maiores et harum ad dolore modi pariatur laborum tempore doloribus aliquid molestiae, vero expedita beatae alias consequuntur blanditiis!"
+}
+
+
+const Show7 = () =>
+{
+    let img_src = document.getElementById("img_7").getAttribute("src");
+    
+    document.getElementById("res").setAttribute("src", img_src)
+    document.getElementById("Lorem").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ducimus facilis amet maiores et harum ad dolore modi pariatur laborum tempore doloribus aliquid molestiae, vero expedita beatae alias consequuntur blanditiis!"
+}
+
+
+const Show8 = () =>
+{
+    let img_src = document.getElementById("img_8").getAttribute("src");
+    
+    document.getElementById("res").setAttribute("src", img_src)
+    document.getElementById("Lorem").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ducimus facilis amet maiores et harum ad dolore modi pariatur laborum tempore doloribus aliquid molestiae, vero expedita beatae alias consequuntur blanditiis!"
+}
+
+const Show9 = () =>
+{
+    let img_src = document.getElementById("img_9").getAttribute("src");
+    
+    document.getElementById("res").setAttribute("src", img_src)
+    document.getElementById("Lorem").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ducimus facilis amet maiores et harum ad dolore modi pariatur laborum tempore doloribus aliquid molestiae, vero expedita beatae alias consequuntur blanditiis!"
+}
+
+
+const Show10 = () =>
+{
+    let img_src = document.getElementById("img_10").getAttribute("src");
+    
+    document.getElementById("res").setAttribute("src", img_src)
+    document.getElementById("Lorem").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ducimus facilis amet maiores et harum ad dolore modi pariatur laborum tempore doloribus aliquid molestiae, vero expedita beatae alias consequuntur blanditiis!"
+}
+
+
+const Show11 = () =>
+{
+    let img_src = document.getElementById("img_11").getAttribute("src");
+    
+    document.getElementById("res").setAttribute("src", img_src)
+    document.getElementById("Lorem").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ducimus facilis amet maiores et harum ad dolore modi pariatur laborum tempore doloribus aliquid molestiae, vero expedita beatae alias consequuntur blanditiis!"
+}
+
+const Show12 = () =>
+{
+    let img_src = document.getElementById("img_12").getAttribute("src");
+    
+    document.getElementById("res").setAttribute("src", img_src)
+    document.getElementById("Lorem").innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum ducimus facilis amet maiores et harum ad dolore modi pariatur laborum tempore doloribus aliquid molestiae, vero expedita beatae alias consequuntur blanditiis!"
+}
